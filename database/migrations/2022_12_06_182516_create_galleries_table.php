ceae9d1c1be4a2b0602ad12561ec67a144324105
Zeljko Klasnja <zeljko.klasnja@gmail.com>
2022-12-20 14:40:43 +0100

    Update description to be nullable

--- database/migrations/2022_12_06_182516_create_galleries_table.php
+++ database/migrations/2022_12_06_182516_create_galleries_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->foreignId('user_id');
             $table->timestamps();
         });
